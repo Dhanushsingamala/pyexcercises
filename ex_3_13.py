5dfc6b42379a53dad71f78c9dd73d552ffe46c2e
Singamala Dhanush Reddy <94308150+Dhanushsingamala@users.noreply.github.com>
2023-04-25 13:01:08 +0530

    Update ex_3_13.py

--- ex_3_13.py
+++ ex_3_13.py
@@ -1,19 +1,25 @@
 import os
 import sys
 
-def cleanup_directory(directory):
-    for path, dirs, files in os.walk(directory):
-        for file in files:
-            if 'tmp' in file:
-                os.remove(os.path.join(path, file))
-
-if len(sys.argv) != 2:
-    print(f'Usage: {sys.argv[0]} <directory>')
+def cleanup_directory(directory, files):
+    for file in files:
+        if 'tmp' in file:
+            path = os.path.join(directory, file)
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                os.rmdir(path)
+#first checks that the user has provided a directory name as a command line argument.
+# If not, it prints an error message and exits
+if len(sys.argv) < 2:
+    print('Usage:', sys.argv[0], '<directory>')
     sys.exit(1)
-
+# If a directory is provided, it checks if the directory name contains the substring "tmp", 
+# and if it does, it prints an error message and exits.
 directory = sys.argv[1]
 if 'tmp' in directory:
-    print('Error: The directory name contains "tmp"')
+    print('Error: The directory', directory, 'contains "tmp"')
     sys.exit(1)
 
-cleanup_directory(directory)
+os.path.walk(directory, cleanup_directory, None)  #THREE ARGS root,function,arg here arg is none since we are using for traverse
+
